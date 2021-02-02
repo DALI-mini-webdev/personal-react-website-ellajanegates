abbca1e32e2aebbc1a344dafd78e9ea2db650d31
ellajanegates <ella.j.gates.24@dartmouth.edu>
2021-02-02 18:44:42 -0500

    add interactivity, navbar, and spinning icon

--- website1/src/App.js
+++ website1/src/App.js
@@ -8,34 +8,40 @@ import horses from './images/IMG_3842.jpg';
 import ridge from './images/IMG_9653.jpg';
 import packTrip from './images/IMG_9099.JPG';
 import lake from './images/IMG_9783.JPG';
-import ropes from './images/IMG_9687.JPG'
+import ropes from './images/IMG_9687.JPG';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <head>
-        <title>Ella Gates</title>
-        <link rel="stylesheet" href="style.css"/>
-    </head>
-    <body>
+      <Navbar />
+    <div>
         <img src={EllaID} alt=""/>
-        <h1 className="blue-text">About Me</h1>
+        <h1>About Me</h1>
         <p>Hi! I’m Ella, a '24 from Brooklyn, NY. I’m planning to major in Computer Science with a second major or minor in Linguistics.</p>
+        <button type="button" onClick={() => alert("hi!")}> Say hi! </button>
         <div>
             <p>In my free time I like to:</p>
-            <ul>
-              <li>cook</li>
-              <img src={cooking} alt=""/>
-              <li>hike</li>
-              <img src={hiking} alt=""/>
-              <li>ski</li>
-              <img src={skiing} alt=""/>
-              <li>ride horses/play polocrosse</li>
-              <img src={horses} alt=""/>
-            </ul>
+              <div className="image">
+                <p>cook</p>
+                <img src={cooking} alt=""/>
+              </div>
+              <div className="image">           
+                <p>hike</p>
+                <img src={hiking} alt=""/>
+              </div>
+              <div className="image">
+                <p>ski</p>
+                <img src={skiing} alt=""/>
+              </div>
+              <div className="image">
+                <p>ride horses/play polocrosse</p>
+                <img src={horses} alt=""/>
+              </div>
           </div>
         <p>I took a gap year last year and worked at an outdoor education center in Colorado and then lived in Mexico City for a few months.</p>
         
+        <div className="imageGallery">
         <div className="gallery">
             <a target="_blank" rel="noreferrer" href={ridge}>
               <img src={ridge} alt="Ridge Walk" width="1000" height="400"/>
@@ -63,8 +69,9 @@ function App() {
             </a>
             <div className="desc">Hanging out in the ropes course</div>
           </div>
+          </div>
+          </div>
 
-          </body>
     </div>
   );
 }
---
+++ website1/src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'About Me',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Photos',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'IDK',
+        url: '#',
+        cName: 'nav-links-mobile',
+    },
+    
+]
