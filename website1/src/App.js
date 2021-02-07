046742d786290132099dce28f63383f4c6e0c582
ellajanegates <ella.j.gates.24@dartmouth.edu>
2021-02-07 14:53:20 -0500

    add caallback functions (One of them isn't quite working... I know you said to get it to compile but I couldn't figure out the problem even with google)'

--- website1/src/App.js
+++ website1/src/App.js
@@ -5,13 +5,21 @@ import cooking from './images/cooking.png';
 import hiking from './images/IMG_9518.JPG';
 import skiing from './images/IMG_0031.JPG';
 import horses from './images/IMG_3842.jpg';
-import ridge from './images/IMG_9653.jpg';
-import packTrip from './images/IMG_9099.JPG';
-import lake from './images/IMG_9783.JPG';
-import ropes from './images/IMG_9687.JPG';
 import Navbar from "./components/Navbar/Navbar";
+import ImageGallery from "./ImageGallery";
 
 function App() {
+  function greeting(name) {
+    alert('Hello ' + name + '!');
+  }
+  
+  function processUserInput(callback) {
+    var name = prompt('Please enter your name.');
+    callback(name);
+  }
+  
+  processUserInput(greeting);
+
   return (
     <div className="App">
       <Navbar />
@@ -20,6 +28,7 @@ function App() {
         <h1>About Me</h1>
         <p>Hi! I’m Ella, a '24 from Brooklyn, NY. I’m planning to major in Computer Science with a second major or minor in Linguistics.</p>
         <button type="button" onClick={() => alert("hi!")}> Say hi! </button>
+        
         <div>
             <p>In my free time I like to:</p>
               <div className="image">
@@ -41,35 +50,7 @@ function App() {
           </div>
         <p>I took a gap year last year and worked at an outdoor education center in Colorado and then lived in Mexico City for a few months.</p>
         
-        <div className="imageGallery">
-        <div className="gallery">
-            <a target="_blank" rel="noreferrer" href={ridge}>
-              <img src={ridge} alt="Ridge Walk" width="1000" height="400"/>
-            </a>
-            <div className="desc">Hiking with coworkers in Colorado</div>
-          </div>
-          
-          <div className="gallery">
-            <a target="_blank" rel="noreferrer" href={packTrip}>
-              <img src={packTrip} alt="Horse" width="600" height="400"/>
-            </a>
-            <div className="desc">Leading a pack trip through the mountains</div>
-          </div>
-          
-          <div className="gallery">
-            <a target="_blank" rel="noreferrer" href={lake}>
-              <img src={lake} alt="Ice Canoeing" width="600" height="400"/>
-            </a>
-            <div className="desc">Breaking up a frozen lake</div>
-          </div>
-          
-          <div className="gallery">
-            <a target="_blank" rel="noreferrer" href={ropes}>
-              <img src={ropes} alt="Ropes Course" width="600" height="400"/>
-            </a>
-            <div className="desc">Hanging out in the ropes course</div>
-          </div>
-          </div>
+        <ImageGallery />
           </div>
 
     </div>
